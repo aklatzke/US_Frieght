a8e52f4440cc590c61e9d5d194ea45767559e2a0
davygxyz <davygxyz@gmail.com>
2016-01-31 04:28:00 -0500

    Initial Commit
    First Commit for US Freight, working on form. Having trouble resizing
    reCaptacha and also need to make the page responsive.

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include"template/header.php"; ?>
+  <div class="col-md-offset-4 col-md-16">
+    <div id='main-text'>
+      <h4>A New Look. A New Design.</h4>
+      <h1>Same US Freight.</h1>
+      <p>
+        This is an exciting time for US Freight! Please bear with us while we build our new
+        website. We're designing a new site for a better experience.
+      </p>
+    </div>
+  </div>
+
+  <div class="col-md-12">
+    <fieldset>
+      <legend>Contact Us</legend>
+      <div class="col-md-offset-1 col-md-22">
+        <form method="POST">
+          <input type="text" class="form-control" placeholder="Text input">
+          <input type="text" class="form-control" placeholder="Text input">
+          <input type="text" class="form-control" placeholder="Text input">
+          <textarea class="form-control" rows="5">Message</textarea>
+          <div class="g-recaptcha" data-sitekey="6Lfp4xYTAAAAAF1yvc7xJh0Md2T0x5u9LuJGSIou"></div>
+          <button type="submit" class="btn btn-custom btn-lg btn-block">Submit</button>
+      </form>
+      </div>
+    </fieldset>
+  </div>
+  <div class="col-md-12">
+    <fieldset>
+      <legend>We're Hiring</legend>
+
+      <div class="col-md-offset-1 col-md-22">
+        <div id='secondary-text'>
+          <p>We’re expanding our team and hiring individuals who are interested in a high earning potential and challenging career in logistics. If you think you have what it takes to help us grow and acheive our goals, apply today.</p>
+          <p>Please submit your resume and cover letter to:</p>
+          <p><a href="mailto:careers@gousfreight.com?Subject=Employment Resume" target="_top">careers@gousfreight.com</a></p>
+        </div>
+      </div>
+    </fieldset>
+  </div>
+
+
+<?php include"template/footer.php"; ?>
---
+++ template/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>US Freight | Welcome</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<!--Javascript for reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<!--END-->
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-6 col-md-6">
+						<img src="img/old-logo.jpg" class="img-responsive" alt="Responsive image"/>
+					</div>
+					<div class="col-xs-16 col-md-offset-1 col-md-9">
+						<img src="img/new-logo.jpg" class="img-responsive" alt="Responsive image"/>
+					</div>
+					<div class="col-xs-24 col-md-offset-2 col-md-6">
+						<p id="company-phn">Call Us <i class="fa fa-phone"></i> <a href="tel:440.331.7447">440.331.7447</a></p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			<div class="row">
